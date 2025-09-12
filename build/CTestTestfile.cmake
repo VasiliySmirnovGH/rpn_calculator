14d2e843f07f90462a084199e3328345d23ce58f
Vasiliy Smirnov <smirnovf08@icloud.com>
2025-09-12 20:53:09 +0300

    Fix gitinore1

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/smi/rpn_calculator
-# Build directory: /home/smi/rpn_calculator/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
